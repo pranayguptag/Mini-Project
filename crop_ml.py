bcdf96794e12d345cebff53306a401bb6cc3112e
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2024-04-12 22:16:23 +0530

    ml file in python

---
+++ crop_ml.py
@@ -0,0 +1,134 @@
+# Importing libraries
+
+from __future__ import print_function
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import classification_report
+from sklearn import metrics
+from sklearn import tree
+import warnings
+warnings.filterwarnings('ignore')
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
+
+from sklearn.naive_bayes import GaussianNB
+
+from sklearn.svm import SVC
+from sklearn.preprocessing import MinMaxScaler
+
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.ensemble import RandomForestClassifier
+
+
+df = pd.read_csv(r"/Users/mayankpant/Documents/Code/GitHub Projects/Hack-X/crop_recommendation.csv")
+
+
+features = df[['N', 'P','K','temperature', 'humidity', 'ph', 'rainfall']]
+target = df['label']
+#features = df[['temperature', 'humidity', 'ph', 'rainfall']]
+labels = df['label']
+
+
+# Initialzing empty lists to append all model's name and corresponding name
+acc = []
+model = []
+
+
+# Splitting into train and test data
+
+
+Xtrain, Xtest, Ytrain, Ytest = train_test_split(features,target,test_size = 0.2,random_state =2)
+
+
+
+
+DecisionTree = DecisionTreeClassifier(criterion="entropy",random_state=2,max_depth=5)
+
+DecisionTree.fit(Xtrain,Ytrain)
+
+predicted_values = DecisionTree.predict(Xtest)
+x = metrics.accuracy_score(Ytest, predicted_values)
+acc.append(x)
+model.append('Decision Tree')
+
+# Cross validation score (Decision Tree)
+score = cross_val_score(DecisionTree, features, target,cv=5)
+
+# Guassian Naive Bayes
+
+NaiveBayes = GaussianNB()
+
+NaiveBayes.fit(Xtrain,Ytrain)
+
+predicted_values = NaiveBayes.predict(Xtest)
+x = metrics.accuracy_score(Ytest, predicted_values)
+acc.append(x)
+model.append('Naive Bayes')
+
+# Cross validation score (NaiveBayes)
+score = cross_val_score(NaiveBayes,features,target,cv=5)
+
+# data normalization with sklearn
+from sklearn.preprocessing import MinMaxScaler
+# fit scaler on training data
+norm = MinMaxScaler().fit(Xtrain)
+X_train_norm = norm.transform(Xtrain)
+# transform testing dataabs
+X_test_norm = norm.transform(Xtest)
+SVM = SVC(kernel='poly', degree=3, C=1)
+SVM.fit(X_train_norm,Ytrain)
+predicted_values = SVM.predict(X_test_norm)
+x = metrics.accuracy_score(Ytest, predicted_values)
+acc.append(x)
+model.append('SVM')
+
+# Logistic Regression
+
+
+LogReg = LogisticRegression(random_state=2)
+
+LogReg.fit(Xtrain,Ytrain)
+
+predicted_values = LogReg.predict(Xtest)
+
+x = metrics.accuracy_score(Ytest, predicted_values)
+acc.append(x)
+model.append('Logistic Regression')
+
+# Cross validation score (Logistic Regression)
+score = cross_val_score(LogReg,features,target,cv=5)
+
+
+
+
+RF = RandomForestClassifier(n_estimators=20, random_state=0)
+RF.fit(Xtrain,Ytrain)
+
+predicted_values = RF.predict(Xtest)
+
+x = metrics.accuracy_score(Ytest, predicted_values)
+acc.append(x)
+model.append('RF')
+
+
+# Cross validation score (Random Forest)
+score = cross_val_score(RF,features,target,cv=5)
+
+# user input from html form
+N = 89
+P = 63
+K = 78
+temp = 50.0
+humidity = 10.0
+ph = 7.0
+rainfall = 10.00
+
+
+data = np.array([[N, P, K, temp,humidity, ph, rainfall]])
+prediction = RF.predict(data)
+print(prediction)
